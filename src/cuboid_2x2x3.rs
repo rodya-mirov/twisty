f720df4f26097cb4d69c1f43fb2644fb93aca3f1
Richard Rast <richard.rast@trinetx.com>
2024-01-02 22:46:50 -0500

    wip: move 2x2x3 move code to owned functions

--- src/cuboid_2x2x3.rs
+++ src/cuboid_2x2x3.rs
@@ -38,108 +38,8 @@ pub struct Cuboid2x2x3 {
     brc: CenterCubelet,
 }
 
-impl SimpleState for Cuboid2x2x3 {
-    fn neighbors<Recv>(&self, to_add: &mut Recv)
-    where
-        Recv: FnMut(Self),
-    {
-        // three neighbors: U2, R2, D2
-        let Self {
-            ufl,
-            ufr,
-            ubl,
-            ubr,
-            dfl,
-            dfr,
-            dbl,
-            dbr,
-            flc,
-            frc,
-            brc,
-        } = *self;
-
-        // U
-        to_add(Self {
-            ufl: ufr,
-            ufr: ubr,
-            ubr: ubl,
-            ubl: ufl,
-            ..*self
-        });
-
-        // U2
-        to_add(Self {
-            ufl: ubr,
-            ubr: ufl,
-            ufr: ubl,
-            ubl: ufr,
-            ..*self
-        });
-
-        // U'
-        to_add(Self {
-            ufl: ubl,
-            ubl: ubr,
-            ubr: ufr,
-            ufr: ufl,
-            ..*self
-        });
-
-        // D
-        to_add(Self {
-            dfl: dbl,
-            dbl: dbr,
-            dbr: dfr,
-            dfr: dfl,
-            ..*self
-        });
-
-        // D2
-        to_add(Self {
-            dfl: dbr,
-            dbr: dfl,
-            dfr: dbl,
-            dbl: dfr,
-            ..*self
-        });
-
-        // D'
-        to_add(Self {
-            dfl: dfr,
-            dfr: dbr,
-            dbr: dbl,
-            dbl: dfl,
-            ..*self
-        });
-
-        // F2
-        to_add(Self {
-            // cycle front corners
-            dfl: ufr,
-            ufr: dfl,
-            dfr: ufl,
-            ufl: dfr,
-            // mess with front two centers
-            flc: frc,
-            frc: flc,
-            ..*self
-        });
-
-        // R2
-        to_add(Self {
-            // cycle right corners
-            dfr: ubr,
-            ubr: dfr,
-            dbr: ufr,
-            ufr: dbr,
-            // mess with right two centers
-            frc: brc,
-            brc: frc,
-            ..*self
-        });
-    }
-
-    fn start() -> Self {
+impl Cuboid2x2x3 {
+    fn solved() -> Self {
         Self {
             ufl: CornerCubelet::UFL,
             ufr: CornerCubelet::UFR,
@@ -153,5 +53,85 @@ impl SimpleState for Cuboid2x2x3 {
             frc: CenterCubelet::FR,
             brc: CenterCubelet::BR,
         }
+    }
+
+    #[inline(always)]
+    fn u(&self) -> Self {
+        Self {
+            ufl: self.ufr,
+            ufr: self.ubr,
+            ubr: self.ubl,
+            ubl: self.ufl,
+            ..*self
+        }
+    }
+
+    #[inline(always)]
+    fn d(&self) -> Self {
+        Self {
+            dfl: self.dbl,
+            dbl: self.dbr,
+            dbr: self.dfr,
+            dfr: self.dfl,
+            ..*self
+        }
+    }
+
+    #[inline(always)]
+    fn f2(&self) -> Self {
+        Self {
+            // cycle front corners
+            dfl: self.ufr,
+            ufr: self.dfl,
+            dfr: self.ufl,
+            ufl: self.dfr,
+            // mess with front two centers
+            flc: self.frc,
+            frc: self.flc,
+            ..*self
+        }
+    }
+
+    #[inline(always)]
+    fn r2(&self) -> Self {
+        Self {
+            // cycle right corners
+            dfr: self.ubr,
+            ubr: self.dfr,
+            dbr: self.ufr,
+            ufr: self.dbr,
+            // mess with right two centers
+            frc: self.brc,
+            brc: self.frc,
+            ..*self
+        }
+    }
+
+}
+
+impl SimpleState for Cuboid2x2x3 {
+    fn neighbors<Recv>(&self, to_add: &mut Recv)
+    where
+        Recv: FnMut(Self),
+    {
+        // U
+        to_add(self.u());
+        to_add(self.u().u());
+        to_add(self.u().u().u());
+
+        // D
+        to_add(self.d());
+        to_add(self.d().d());
+        to_add(self.d().d().d());
+
+        // F2
+        to_add(self.f2());
+
+        // R2
+        to_add(self.r2());
+    }
+
+    fn start() -> Self {
+        Self::solved()
     }
 }
